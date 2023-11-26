d786e67d57cb9ec5ea023e71c5ba5ebc50e3a55b
brianmccann <bmccann36@gmail.com>
2023-11-26 09:35:39 -0500

    Add initial project setup with CloudFront canary release mechanism

---
+++ edgeHandlers/originReqHandler.js
@@ -0,0 +1,26 @@
+exports.handler = async (event) => {
+
+  const req = event.Records[0].cf.request
+  let previousWeight = 0
+
+  if (req.headers.cookie) {
+    // Get the previous weight from cookie
+    previousWeight = +(req.headers.cookie.find(c => c.value.startsWith('${ReleaseName}=')) || '0').value.match('=(\\d+)')[1]
+  }
+
+  let isNext = previousWeight == 999 // 999 means the user is already on next version
+
+  if (!isNext) {
+    // Calculate the new weight considering the previous one
+    const newWeight = (process.env.RELEASE_WEIGHT - previousWeight) / (100 - previousWeight)
+
+    // Randomly decide on app version based on weight probability
+    isNext = Math.random() < newWeight
+  }
+  //
+  // if (isNext) {
+  //   // rewrite request origin to the "next bucket"
+  //   req.headers.host = [{key: 'host', value: req.origin.s3.domainName = '${NextBucket}'}]
+  // }
+  // return req
+}
