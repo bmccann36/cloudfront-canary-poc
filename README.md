3a7ada8ac4d112c2675e3c488121534ea92e1a51
brianmccann <bmccann36@gmail.com>
2023-11-26 10:49:13 -0500

    Refactor origin request handler to use hardcoded values and improve logging

---
+++ README.md
@@ -0,0 +1,3 @@
+
+
+sam local invoke RequestLambda -e sam-local/test-req-event.json --env-vars sam-local/local-env.json
--- edgeHandlers/originReqHandler.js
+++ edgeHandlers/originReqHandler.js
@@ -1,28 +1,35 @@
 exports.handler = async (event) => {
 
-  console.log(JSON.stringify(event))
+  // console.log(JSON.stringify(event))
 
   const req = event.Records[0].cf.request
   let previousWeight = 0
 
   if (req.headers.cookie) {
     // Get the previous weight from cookie
-    previousWeight = +(req.headers.cookie.find(c => c.value.startsWith(`${process.env.RELEASE_NAME}=`)) || '0').value.match('=(\\d+)')[1]
+    previousWeight = +(req.headers.cookie.find(c => c.value.startsWith('v2=')) || '0').value.match('=(\\d+)')[1]
   }
 
-  let isNext = previousWeight == 999 // 999 means the user is already on next version
+  let serveNextVersion = previousWeight == 999 // 999 means the user is already on next version
 
-  if (!isNext) {
+  if (!serveNextVersion) {
+
+    console.log('next not set')
     // Calculate the new weight considering the previous one
-    const newWeight = (process.env.RELEASE_WEIGHT - previousWeight) / (100 - previousWeight)
+    const newWeight = (50 - previousWeight) / (100 - previousWeight)
 
     // Randomly decide on app version based on weight probability
-    isNext = Math.random() < newWeight
+    serveNextVersion = Math.random() < newWeight
   }
+  console.log('>>serveNextVersion', serveNextVersion);
 
-  if (isNext) {
+  if (serveNextVersion) {
     // rewrite request origin to the "next bucket"
-    req.headers.host = [{key: 'host', value: req.origin.s3.domainName = process.env.NEXT_BUCKET}]
+    req.headers.host = [{
+      key: 'host',
+      value: req.origin.s3.domainName = 'cloudfront-canary-poc-nextbucket-x2idzmcx1daj.s3.us-east-1.amazonaws.com'
+    }]
+    // or else it will be: cloudfront-canary-poc-currentbucket-9tteolqz6fi8.s3.us-east-1.amazonaws.com
   }
   return req
 }
