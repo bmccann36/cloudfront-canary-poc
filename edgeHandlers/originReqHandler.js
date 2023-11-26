42396fcdbc976137abfc981ff8a06c96add0c1c9
brianmccann <bmccann36@gmail.com>
2023-11-26 09:49:02 -0500

    Add .gitignore for Node projects and integrate environment variables in edge handlers

--- edgeHandlers/originReqHandler.js
+++ edgeHandlers/originReqHandler.js
@@ -1,11 +1,13 @@
 exports.handler = async (event) => {
 
+  console.log(JSON.stringify(event))
+
   const req = event.Records[0].cf.request
   let previousWeight = 0
 
   if (req.headers.cookie) {
     // Get the previous weight from cookie
-    previousWeight = +(req.headers.cookie.find(c => c.value.startsWith('${ReleaseName}=')) || '0').value.match('=(\\d+)')[1]
+    previousWeight = +(req.headers.cookie.find(c => c.value.startsWith(`${process.env.RELEASE_NAME}=`)) || '0').value.match('=(\\d+)')[1]
   }
 
   let isNext = previousWeight == 999 // 999 means the user is already on next version
@@ -17,10 +19,10 @@ exports.handler = async (event) => {
     // Randomly decide on app version based on weight probability
     isNext = Math.random() < newWeight
   }
-  //
-  // if (isNext) {
-  //   // rewrite request origin to the "next bucket"
-  //   req.headers.host = [{key: 'host', value: req.origin.s3.domainName = '${NextBucket}'}]
-  // }
-  // return req
+
+  if (isNext) {
+    // rewrite request origin to the "next bucket"
+    req.headers.host = [{key: 'host', value: req.origin.s3.domainName = process.env.NEXT_BUCKET}]
+  }
+  return req
 }
